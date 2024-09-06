c57f407fc7584437b80a788a6c0ba20e343fd872
Prashant <badiger20@gmail.com>
2024-09-06 08:16:04 +0530

    hi

---
+++ src/Sept/ex_06092024/HelloATB.java
@@ -0,0 +1,10 @@
+package Sept.ex_06092024;
+
+public class HelloATB {
+
+    public static void main(String[] org){
+        System.out.println("hi Prashant");
+        System.out.println(2+2);
+
+    }
+}
